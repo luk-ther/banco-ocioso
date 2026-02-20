e33f6c448be1b8dc95e5756474154d50001d66cc
luk-ther <nubankservicosalt@gmail.com>
2026-02-20 16:28:37 -0300

    chore: configure supabase project url and publishable key

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
-﻿window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+﻿window.SUPABASE_URL = "https://gvqkxgxkzfwrcoxutcsy.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_2RhxJY98Zp3dL9QcfPh-kw_cyblIhx-";
